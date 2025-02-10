1923f1adbaf511cc68cc3fba1e54f4b913d32cae
AzeezAAlimi <azeezaalimi@gmail.com>
2025-02-10 13:50:46 +0100

    test

--- src/tests/uiandapi/uiexample2.spec.ts
+++ src/tests/uiandapi/uiexample2.spec.ts
@@ -1,17 +1,17 @@
 import { test, expect } from '@playwright/test';
 
 test('Second', async ({ page }) => {
-  await page.goto('https://rahulshettyacademy.com/client/auth/login');
-  console.log(await page.title());
+  await page.goto('https://rahulshettyacademy.com/client');
   await page
-    .getByRole('textbox', { name: 'email@example.com' })
+    .locator('input[id="userEmail"]')
     .fill('qaairbnb0@gmail.com');
-  await page.getByRole('textbox', { name: 'enter your passsword' }).fill('Test1234?');
+  await page.locator('input[id="userPassword"]').fill('Test1234?');
   await page.getByRole('button', { name: 'Login' }).click();
   await page.waitForURL('https://rahulshettyacademy.com/client/dashboard/dash');
   expect(page.url()).toBe(
     'https://rahulshettyacademy.com/client/dashboard/dash',
   );
+  await page.waitForLoadState('networkidle')
   const cardTitles = page.locator('.card-body b');
   console.log(await cardTitles.nth(0).textContent());
   const allTitles = await cardTitles.allTextContents();
